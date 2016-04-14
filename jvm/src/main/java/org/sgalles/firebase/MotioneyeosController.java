24fa6c2bb114a0a56e4c1334ef8b2b116c31945e
Stephane Gallès <stephane.galles@gmail.com>
2016-04-14 23:20:43 +0200

    added find for motioneyeos service

--- jvm/src/main/java/org/sgalles/firebase/MotioneyeosController.java
+++ jvm/src/main/java/org/sgalles/firebase/MotioneyeosController.java
@@ -1,5 +1,7 @@
 package org.sgalles.firebase;
 
+import java.io.File;
+import java.io.FilenameFilter;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.atomic.AtomicReference;
@@ -21,19 +23,27 @@ public class MotioneyeosController {
 	private static final String RUNNING_CHILD = "running";
 	private static final String SWITCH_CHILD = "switch";
 	
-	
-
-	
 	private AtomicReference<Boolean> motioneyeosRunning = new AtomicReference<>(null);
 	private AtomicReference<Boolean> firewallRunning = new AtomicReference<>(null);
 	
 	private Firebase ref = null;
+	private final File motionEyeOsService;
 
 	public static void main(String[] args) throws Exception {
 		MotioneyeosController controller = new MotioneyeosController();
 		controller.initFirewall();
 		controller.initFirebase();
 		controller.loop();
+	}
+	
+	public MotioneyeosController(){
+		motionEyeOsService = findMotionEyeOsService();
+	}
+	
+	private static File findMotionEyeOsService(){
+		File servicesDir = new File("/etc/init.d");
+		FilenameFilter findMotionEyeOsService = (File parent, String name) ->  name.endsWith("motioneye");
+		return new File(servicesDir, servicesDir.list(findMotionEyeOsService)[0]);
 	}
 	
 	public void initFirewall(){
@@ -157,12 +167,12 @@ public class MotioneyeosController {
 			if(Boolean.FALSE.equals(motioneyeosRunning.get())){
 				System.out.println("Starting motioneye...");
 				new ProcessExecutor()
-				.command("/etc/init.d/S85motioneye", "start")
+				.command(motionEyeOsService.getAbsolutePath(), "start")
 				.start();
 			}else{
 				System.out.println("Stopping motioneye...");
 				new ProcessExecutor()
-				.command("/etc/init.d/S85motioneye", "stop")
+				.command(motionEyeOsService.getAbsolutePath(), "stop")
 				.start();
 			}
 		}catch(Exception e){
